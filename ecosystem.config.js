3a49f93060c98613894609f0d1484542b349022b
kitehack21 <billy_irianto@yahoo.com>
2020-02-11 00:29:54 +0700

    added pm2 ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'FanatticAPI',
+      script: './index.js',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 4771,
+      },
+      env_test: {
+        NODE_ENV: 'test',
+        PORT: 4771,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 4771,
+      },
+      time: true,
+    },
+  ],
+};
