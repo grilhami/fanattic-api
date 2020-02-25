085f66af60aa92f0a3cd40a5b56e6d7e06b0b2c9
kitehack21 <billy_irianto@yahoo.com>
2020-02-25 16:41:01 +0700

    Update merchandise.js
    fixed typo

--- models/merchandise.js
+++ models/merchandise.js
@@ -9,11 +9,11 @@ module.exports = (sequelize, DataTypes) => {
   );
   merchandise.associate = models => {
     // associations can be defined here
-    merchandise.hasMany(models.merchanise_images, {
+    merchandise.hasMany(models.merchandise_images, {
       forignKey: 'merchandiseId',
       as: 'images',
     });
-    merchandise.hasMany(models.merchanise_links, {
+    merchandise.hasMany(models.merchandise_links, {
       forignKey: 'merchandiseId',
       as: 'links',
     });
