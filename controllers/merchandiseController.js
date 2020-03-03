ebaf38e0e3c778fe8c7584838b87144be72c403d
kitehack21 <billy_irianto@yahoo.com>
2020-03-03 21:27:37 +0700

    merchandise routes and controllers

---
+++ controllers/merchandiseController.js
@@ -0,0 +1,34 @@
+const {
+  merchandise,
+  merchandise_images,
+  merchandise_links,
+} = require('../models');
+const { errorHandler } = require('../helpers');
+
+module.exports = {
+  getAll: (req, res) => {
+    merchandise
+      .findAll({
+        attributes: ['id', 'name', 'price', 'description'],
+        include: [
+          {
+            model: merchandise_links,
+            attributes: ['id', 'url'],
+            as: 'links',
+          },
+          {
+            model: merchandise_images,
+            attributes: ['id', 'url'],
+            as: 'images',
+          },
+        ],
+      })
+      .then(data =>
+        res.status(200).json({
+          message: 'get merchandise',
+          data,
+        }),
+      )
+      .catch(err => errorHandler(err));
+  },
+};
--- models/merchandise.js
+++ models/merchandise.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       name: DataTypes.STRING,
       price: DataTypes.INTEGER,
+      description: DataTypes.TEXT,
     },
     {},
   );
