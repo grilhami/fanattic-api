6857dc350aa0f5c61b1161563a3b0438fa2d9219
ericanthonywu <ericanthonywu89@gmail.com>
2020-03-05 17:11:42 +0700

    change token handler in middleware due express-bearer-token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // eslint-disable-next-line consistent-return
 exports.authMiddleware = (req, res, next) => {
-  const { token } = req.body;
+  const { token } = req;
   if (!token) return res.status(400).json();
   jwt.verify(token, process.env.JWTTOKEN, (err, data) => {
     if (err) {
